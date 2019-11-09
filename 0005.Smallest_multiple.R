0d9591ffee86156ac93793b6c2f1d829c49d655a
Owen Thompson <octhompson19@gmail.com>
2019-11-08 19:30:54 -0600

    Create 0005.Smallest_multiple.R

---
+++ 0005.Smallest_multiple.R
@@ -0,0 +1,29 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+is_prime <- function(x) {
+  if (x == 1) {
+    flag <- FALSE
+  }
+  if (x == 2 | x == 3) {
+    flag <- TRUE
+  } else {
+    flag <- !any(x %% 2:floor(sqrt(x)) == 0)
+  }
+  return(flag)
+}
+
+
+prime_facts <- name <- function(x) {
+  potential_factors <- 2:floor(sqrt(x))
+  factors <- potential_factors[x %% potential_factors == 0]
+  prime_factors <- factors[unlist(lapply(factors, is_prime))]
+  prime_factors
+}
+
+
+all <- 1:20
+
+prime <- all[map(1:20, is_prime) %>% unlist()]
+length(prime)
+prod(prime) * length(prime)
